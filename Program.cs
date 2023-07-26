1a4d113deeca9dfbeaf486bc3793b2144cb53d8e
Devaka <devaka.dabare@immunify.me>
2023-07-26 17:23:41 +0530

    initilizing

---
+++ Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add CORS services.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowMyOrigin",
+    builder =>
+    {
+        builder.WithOrigins(
+            "http://localhost:3000",
+            "https://fitnessappweb.z13.web.core.windows.net"
+            ) // React Web app running URL
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+// Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add authentication services
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "devakadabare",
+            ValidAudience = "devakadabare",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ThisIsMySuperSecretKeyForFitnessAppInMyMSCourseWork"))
+        };
+    });
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseCors("AllowMyOrigin"); // Use the CORS policy
+
+app.UseAuthentication(); // Use authentication middleware
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.Run();
